38fffd6a27d64e4e6124607222988854816d093c
Przegiety <t.przegietka@gmail.com>
2024-07-20 12:17:24 +0200

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance { get; private set; }
+    public int Health { get; set; }
+
+    private void Awake() {
+        Instance = this;
+        
+    }
+    public void StartGame() {
+        Health = 5;
+    }
+}
+
---
+++ Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour {
+    public void TakeDamage(Unit unit) {
+        unit.gameObject.SetActive(false);
+        GameManager.Instance.Health--;
+        Debug.Log("damage");
+    }
+}
+
