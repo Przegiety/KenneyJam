7b2670cffed5020d89c1c668c4ad140ed1a54de3
Przegiety <t.przegietka@gmail.com>
2024-07-21 09:36:32 +0200

    UI

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -11,6 +11,8 @@ namespace Jam {
         [SerializeField] private TextMeshProUGUI _healthText;
         [SerializeField] private TextMeshProUGUI _moneyText;
         [SerializeField] private TextMeshProUGUI _scoreText;
+        [SerializeField] private List<GameObject> _hearts;
+        [SerializeField] private List<GameObject> _money;
         private void Start() {
             GameManager.Instance.HealthChanged += OnHealthChanged;
             GameManager.Instance.MoneyChanged += OnMoneyChanged;
@@ -28,9 +30,17 @@ namespace Jam {
         }
         private void OnHealthChanged() {
             _healthText.text = "Lives: " + GameManager.Instance.Health;
+            int health = GameManager.Instance.Health;
+            for (int i = 0; i < _hearts.Count; i++) {
+                _hearts[i].SetActive(i < health);
+            }
         }
         private void OnMoneyChanged() {
             _moneyText.text = "Money: " + GameManager.Instance.Money;
+            int money = GameManager.Instance.Money;
+            for (int i = 0; i < _money.Count; i++) {
+                _money[i].SetActive(i < money);
+            }
         }
         private void OnScoreChanged() {
             _scoreText.text = GameManager.Instance.Score.ToString();
